7d15b1a8d38db8e1df4cf68cc6878a8faad0f4d4
AnkhLoft <helderemanuelferreira@gmail.com>
2022-09-04 17:11:03 +0100

    feat: Add component header to the project

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar color="primary">
+    <template #prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Expenses Tracker</v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EtHeader',
+})
+</script>
